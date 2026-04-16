38d8a6141af8ab449d20128a3e1c79b49e1887bf
elliotmiles <elliotmiles@hotmail.com>
2026-04-16 17:22:09 +0100

    Add ros2_control for simulation

--- src/wd_simulation/launch/launch_sim.launch.py
+++ src/wd_simulation/launch/launch_sim.launch.py
@@ -62,7 +62,21 @@ def generate_launch_description():
         ]
     )
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diff_cont",
+            '--controller-ros-args',
+            '-r /diff_cont/cmd_vel:=/cmd_vel'
+        ],
+    )
 
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
     # Launch them all!
     return LaunchDescription([
@@ -71,4 +85,6 @@ def generate_launch_description():
         gazebo,
         spawn_entity,
         ros_gz_bridge,
+        diff_drive_spawner,
+        joint_broad_spawner,
     ])
